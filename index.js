5e4fac8c07cc51e7073ef1887f561f6cf2508b2e
Michael Coons <mcoons67@gmail.com>
2021-11-28 23:14:27 -0700

    removed console log

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
 (() => {
 
     let _timeoutID, _dataLoaded, _jsonData;
-    let _atomList = new Map();
+    const _atomList = new Map();
     let _container = document.getElementById("container");
     let _tabActive = true;
-    let randomRange = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+    const randomRange = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
     document.addEventListener('visibilitychange', () => {
         if (document.hidden) {
-            // tab is now inactive
-            // temporarily clear timer using clearInterval() / clearTimeout()
             _tabActive = false;
             clearTimeout(_timeoutID);
             _atomList.forEach( a => {
                 a.style.animationPlayState = 'paused';
-
-                // a.classList.remove("animation_playing");
-                // a.classList.add("animation_paused");
             });
         } else {
-            // tab is active again
-            // restart timers
             _timeoutID = setTimeout(spawnAtom, 1500);
             _tabActive = true;
             _atomList.forEach( a => {
                 a.style.animationPlayState = 'running';
-                // a.classList.remove("animation_paused");
-                // a.classList.add("animation_playing");
             });
         }
     });
@@ -36,8 +27,8 @@
         _atomList.delete(_atomList.keys().next().value);
     }
     
-    var spawnAtom = () => {
-        console.log('Atom spawning');
+    const spawnAtom = () => {
+        // console.log('Atom spawning');
 
         let r = randomRange(0, _jsonData.length - 5);
         while (_atomList.has(r)) {
@@ -76,7 +67,7 @@
             _timeoutID = setTimeout(spawnAtom, 1500);
     }
 
-    var xmlhttp = new XMLHttpRequest();
+    const xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             _jsonData = JSON.parse(this.responseText);
